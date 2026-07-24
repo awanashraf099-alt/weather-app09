acd3de260a6383ec10809c95b84f5ffdb7f510fb
irfan ashraf <awanashraf099@gmail.com>
2026-07-25 02:07:03 +0500

    Add icons to service worker precache list

--- service-worker.js
+++ service-worker.js
@@ -5,9 +5,10 @@ const assets = [
     "./index.html",
     "./style.css",
     "./script.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
 ];
-
 
 // Install Service Worker
 self.addEventListener("install", event => {
