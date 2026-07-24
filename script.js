f00328394c51eeb5e33c4f7140f3a96e679841ae
irfan ashraf <awanashraf099@gmail.com>
2026-07-25 00:47:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -168,4 +168,38 @@ if ("serviceWorker" in navigator) {
     });
 
 }
+// =========================
+// INSTALL PWA
+// =========================
 
+let deferredPrompt;
+
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+
+    e.preventDefault();
+
+    deferredPrompt = e;
+
+    installBtn.style.display = "block";
+
+});
+
+installBtn.addEventListener("click", async () => {
+
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt();
+
+    const { outcome } = await deferredPrompt.userChoice;
+
+    if (outcome === "accepted") {
+        console.log("App Installed");
+    }
+
+    deferredPrompt = null;
+
+    installBtn.style.display = "none";
+
+});
