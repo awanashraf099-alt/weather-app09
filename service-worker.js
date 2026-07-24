6a04d041b54cd25e1e1e17762f8f9a8b78d05e0a
irfan ashraf <awanashraf099@gmail.com>
2026-07-24 22:50:22 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+Enterconst cacheName = "weather-app-v1";
+
+const assets = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+
+// Install Service Worker
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+
+});
+
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys().then(keys => {
+
+            return Promise.all(
+                keys.map(key => {
+
+                    if(key !== cacheName){
+                        return caches.delete(key);
+                    }
+
+                })
+            );
+
+        })
+    );
+
+});
+
+
+// Fetch Offline Support
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
