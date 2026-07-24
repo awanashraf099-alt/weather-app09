68133aa9520c370bd31c16fdba5cf28fbf1a2dbc
irfan ashraf <awanashraf099@gmail.com>
2026-07-24 22:53:51 +0500

    Register service worker in script.js
    Add service worker registration to script.js

--- script.js
+++ script.js
@@ -155,5 +155,17 @@ function getCurrentLocation(){
     );
 
         }
+// Register Service Worker
 
+if ("serviceWorker" in navigator) {
+
+    navigator.serviceWorker.register("service-worker.js")
+    .then(() => {
+        console.log("Service Worker Registered");
+    })
+    .catch(error => {
+        console.log("Service Worker Error:", error);
+    });
+
+}
 
